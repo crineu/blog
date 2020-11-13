67ecbbd648f42f2b535108545926ce006bf7b1dd
Crineu Tres <crineu@gmail.com>
2020-11-12 21:18:13 -0300

    Interpola criação do index e posts; script v0.6

--- convert_md_to_html.sh
+++ convert_md_to_html.sh
@@ -1,75 +1,84 @@
 #!/bin/bash
-
-# for MD_FILE in "markdown/posts/2015-04-23-the-fountainhead.md"
-for MD_FILE in markdown/posts/*
-do
-     MD_META=$(sed   '8q' <"${MD_FILE}")
-  MD_CONTENT=$(sed '1,8d' <"${MD_FILE}")
-
-    TITLE=$(echo "${MD_META}" | awk 'BEGIN{FS=": "} /title/{print $2}')
-  EXCERPT=$(echo "${MD_META}" | awk 'BEGIN{FS=": "} /excerpt/{print $2}')
-     DATE=$(echo "${MD_META}" | awk '/date_published/{print $2}' | cut -d'T' -f1)
-     YEAR=$(echo "${DATE}"    | cut -d'-' -f1)
-
-  HTML_FILE=$(echo $MD_FILE |\
-            sed -e "s/^markdown\/posts/docs\/${YEAR}/" |\
-            sed -e 's/md$/html/')
-
-
-  echo "Gerando arquivo ${HTML_FILE}"
-
-  echo '<!DOCTYPE html>'   >  "${HTML_FILE}"
-  echo '<html>'            >> "${HTML_FILE}"
-  (cat docs/head.html | sed -e "s/__TITLE__/${TITLE}/") >> "${HTML_FILE}"
-  echo ''                  >> "${HTML_FILE}"
-  echo '<body>'            >> "${HTML_FILE}"
-  echo '<div class="pad2y limiter content">' >> "${HTML_FILE}"
-  echo ''                  >> "${HTML_FILE}"
-  echo "<h2>${TITLE}</h2>" >> "${HTML_FILE}"
-  echo "<p>${EXCERPT}</p>" >> "${HTML_FILE}"
-  echo ''                  >> "${HTML_FILE}"
-  (cmark --validate-utf8 --smart --to html <(echo "${MD_CONTENT}"))  >> "${HTML_FILE}"
-  echo "</div>"            >> "${HTML_FILE}"
-  echo "</body>"           >> "${HTML_FILE}"
-  echo "</html>"           >> "${HTML_FILE}"
-done
-
-
-
-# ----------------------------------------------
-# creating the index.html file
-# ----------------------------------------------
+COMMON_HEADER="docs/head.html"
 INDEX_FILE="docs/index.html"
-echo "Criando índice de artigos em ${INDEX_FILE}"
-
-echo -e "<!DOCTYPE html>" \
-        "\n<html>"        > "${INDEX_FILE}"
-(cat docs/head.html | sed -e "s/__TITLE__/Índice/") >> "${INDEX_FILE}"
-echo -e "\n<body>"  \
-        "\n\t<div class=\"pad2y limiter content writing nu\">" \
-        "\n"              >> "${INDEX_FILE}"
 
 
+# ----------------------------------------------
+# Primeira parte do index.html
+# ----------------------------------------------
+echo "Criando índice em ${INDEX_FILE}"
+HEADER=$(cat "${COMMON_HEADER}" | sed -e "s/__TITLE__/Índice/")
+
+cat <<_INICIO_INDEX_ > "${INDEX_FILE}"
+<!DOCTYPE html>
+<html>
+${HEADER}
+<body>
+  <main class="index">
+	  <section class="article-list">
+	    <h1>Meine blog</h1>
+_INICIO_INDEX_
+
+
+# ----------------------------------------------
+# INÍCIO Laço cada arquivo Markdown em markdown/posts
+#        por ordem de criação
+# ----------------------------------------------
+# for MD_FILE in "markdown/posts/2015-04-15-ruby-blocks-lambdas.md"
+# for MD_FILE in markdown/posts/*
 for MD_FILE in `find markdown/posts -type f | sort -r`
 do
-  MD_META=$(sed '8q' <"${MD_FILE}")
-  TITLE=$(echo "${MD_META}" | awk 'BEGIN{FS=": "} /title/{print $2}')
-   DATE=$(echo "${MD_META}" | awk '/date_published/{print $2}' | cut -d'T' -f1)
-   YEAR=$(echo "${DATE}"    | cut -d'-' -f1)
+  # Split source file into Metadata and Content
+  MD_META=$(sed      '8q' <"${MD_FILE}")
+  MD_CONTENT=$(sed '1,8d' <"${MD_FILE}")
 
-  REL_LINK=$(echo $MD_FILE |\
-             sed -e "s/^markdown\/posts/${YEAR}/" |\
-             sed -e 's/md$/html/')
+  # Extract values
+  TITLE=$(echo   "${MD_META}" | awk 'BEGIN{FS=": "} /title/{print $2}')
+  EXCERPT=$(echo "${MD_META}" | awk 'BEGIN{FS=": "} /excerpt/{print $2}')
+  DATE=$(echo    "${MD_META}" | awk '/date_published/{print $2}' | cut -d'T' -f1)  # 2015-02-18T20:58:49.000Z
+  YEAR=$(echo    "${DATE}"    | cut -d'-' -f1)                                     # 2015-02-18
+  HEADER=$(cat "${COMMON_HEADER}" | sed -e "s/__TITLE__/${TITLE}/")
+  ARTICLE=$(cmark --validate-utf8 --smart --to html <(echo "${MD_CONTENT}"))
 
-  echo -e "\t\t"         \
-          "<div><a href=\"${REL_LINK}\">${TITLE}</a></div>"   \
-          '<time>'       \
-          ${DATE}        \
-          '</time>'      >> "${INDEX_FILE}"
+  HTML_FILE=$(echo "${MD_FILE}" | sed -e "s/^markdown\/posts/docs\/${YEAR}/" | sed -e "s/md$/html/")
+  echo "Criando ${HTML_FILE}"
+
+  cat << _POST_ > "${HTML_FILE}"
+<!DOCTYPE html>
+<html>
+${HEADER}
+<body>
+  <h1>${TITLE}<small>${DATE} - ${EXCERPT}</small></h1>
+  <main>
+    <article>
+      ${ARTICLE}
+    </article>
+  </main>
+</body>
+</html>
+_POST_
+
+  # Cria referência em index.html
+  REL_LINK=$(echo "${HTML_FILE}" | sed -e "s/^docs\///")
+  cat <<_POST_LINK_ >> "${INDEX_FILE}"
+  <div class="article">
+    <span class="date">${DATE}</span>
+    <a href="${REL_LINK}">${TITLE}</a>
+  </div>
+_POST_LINK_
+
 done
+# ----------------------------------------------
+# FIM Laço cada arquivo Markdown em markdown/posts
+# ----------------------------------------------
 
 
-echo -e "\n"       \
-        "\t</div>" \
-        "\n</body>"  \
-        "\n</html>"  >> "${INDEX_FILE}"
+# ----------------------------------------------
+# Parte final do index.html
+# ----------------------------------------------
+cat <<_FIM_INDEX_ >> "${INDEX_FILE}"
+    </section>
+  </main>
+</body>
+</html>
+_FIM_INDEX_
